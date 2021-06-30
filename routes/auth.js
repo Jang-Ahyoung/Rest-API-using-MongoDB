f467194cb3d87e1de7206e4057124807c04a50d2
Jang-Ahyoung <wegotthelove27@gmail.com>
2021-06-30 13:17:37 +0900

    User 모델 연결 & 디비 비밀번호값 암호화 저장

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,27 @@
 const router = require("express").Router();
-router.get("/", (req, res) => {
-    res.send("AuthRouter");
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+//REGISTER
+router.post("/register", async (req, res) => {
+    try {
+        // generate new password
+        const salt = await bcrypt.genSalt(10);
+        const hasshedPassword = await bcrypt.hash(req.body.password, salt); // 새로운 비밀번호 생성
+
+        // create new user
+        const newUser = await new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hasshedPassword,
+        });
+
+        // save user and return response
+        const user = await newUser.save();
+        res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+    }
 })
 
 module.exports = router;
